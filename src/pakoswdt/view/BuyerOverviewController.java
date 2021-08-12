4ae30125f7f9e458755ea2db4393210e1187f35f
mariasupinska <marysupinska@gmail.com>
2021-08-12 16:21:17 +0200

    Attaching fx:id and OnAction in BuyerOverview.

--- src/pakoswdt/view/BuyerOverviewController.java
+++ src/pakoswdt/view/BuyerOverviewController.java
@@ -1,11 +1,49 @@
 package pakoswdt.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import pakoswdt.MainApp;
-import pakoswdt.model.Data;
+import pakoswdt.model.Buyer;
+import pakoswdt.model.Vehicle;
 
 public class BuyerOverviewController {
     private MainApp mainApp;
+
+    @FXML
+    private ChoiceBox<Buyer> buyers;
+
+    @FXML
+    private TextField name;
+    @FXML
+    private TextField street;
+    @FXML
+    private TextField city;
+    @FXML
+    private TextField postalCode;
+    @FXML
+    private TextField country;
+    @FXML
+    private TextField nip;
+    @FXML
+    private DatePicker cargoDeliveryDate;
+    @FXML
+    private ChoiceBox<Vehicle> vehicles;
+
+    @FXML
+    private TextField deliveryStreet;
+    @FXML
+    private TextField deliveryCity;
+    @FXML
+    private TextField deliveryPostalCode;
+    @FXML
+    private TextField deliveryCountry;
+    @FXML
+    private TextField personRetrieving;
+    @FXML
+    private TextField personConfirming;
+
 
     public void setMainApp(MainApp mainApp) {
         this.mainApp = mainApp;
@@ -13,7 +51,27 @@ public class BuyerOverviewController {
 
     @FXML
     public void initialize() {
-       // Data.getSeller().getName().setValue("XD");
+
+    }
+
+    @FXML
+    private void handleAddCompany() {
+
+    }
+
+    @FXML
+    private void handleDeleteCompany() {
+
+    }
+
+    @FXML
+    private void handleAddVehicle() {
+
+    }
+
+    @FXML
+    private void handleDeleteVehicle() {
+
     }
 
     @FXML
