f482e8bff3180cfa4162a7770da502e0e0f34273
mariasupinska <marysupinska@gmail.com>
2023-01-16 17:19:32 +0100

    Fixed troublesome string to double parsing.

--- src/main/java/pakoswdt/model/SimpleDoublePropertyConverter.java
+++ src/main/java/pakoswdt/model/SimpleDoublePropertyConverter.java
@@ -2,14 +2,21 @@ package pakoswdt.model;
 
 import com.opencsv.bean.AbstractBeanField;
 import javafx.beans.property.SimpleDoubleProperty;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.math.NumberUtils;
 
+@Slf4j
 public class SimpleDoublePropertyConverter extends AbstractBeanField {
+
     @Override
     protected Object convert(String s) {
         if ( s == null) return null;
-        String replace = s.replace(",", ".");
-        if (!NumberUtils.isParsable(replace.trim())) return null;
+        String replaceSpaces = s.replaceAll("\\p{Z}","");
+        String replace = replaceSpaces.replace(",", ".");
+        if (!NumberUtils.isParsable(replace.trim())) {
+            log.error("Couldn't parse the value of " + s);
+            return null;
+        }
         return new SimpleDoubleProperty(Double.parseDouble(replace.trim()));
     }
 }
